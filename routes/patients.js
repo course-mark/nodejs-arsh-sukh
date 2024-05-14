bcb77104fc6317348754cdfcb3bdc88a349a926d
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-05-14 17:06:43 +0530

    introduction to aggregation pipelines
    Co-authored-by: sukhleensandhu <sukhleensandhu@users.noreply.github.com>

--- routes/patients.js
+++ routes/patients.js
@@ -2,15 +2,10 @@ var express = require('express');
 var router = express.Router();
 const patientServices= require("../services/patient")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 // Create patient
 router.post("/",patientServices.createPatient)
 
 //get patient
-
 router.get('/',patientServices.getPatient)
+
 module.exports = router;
--- services/patient.js
+++ services/patient.js
@@ -9,16 +9,85 @@ const createPatient = async (req, res, next) => {
   });
 };
 
-const getPatient = async (req, res, next) => {
-  const getPatient= await patientModel.find()
-  res.status(200).json(
-      {data:getPatient,
-          success:true
-      
-})};
+// const getPatient = async (req, res, next) => {
+//   const getPatient = await patientModel.find();
+//   const adultPatients = getPatient.filter((patient) => patient.age <= 18); // un-optimized
+//   res.status(200).json({ data: adultPatients, success: true });
+// };
 
-module.exports = { 
-    createPatient: createPatient,
-    getPatient: getPatient,
-    name:"manjot"
- };
+const getPatientWithAgeAggregation = async (req, res, next) => {
+    // to get all patients with age 18
+    // const getPatients = await patientModel.aggregate([
+    //     {
+    //         $match: {
+    //             age:{
+    //                 $eq: 18
+    //             }
+    //         }
+    //     }
+    // ])
+
+    // to get all patients with age less than 18
+    // const getPatients = await patientModel.aggregate([
+    //     {
+    //         $match: {
+    //             age:{
+    //                 $lt: 18
+    //             }
+    //         }
+    //     }
+    // ])
+
+    // to get all patients with age less than or equal to 18
+    // const getPatients = await patientModel.aggregate([
+    //     {
+    //         $match: {
+    //             age:{
+    //                 $lte: 18
+    //             }
+    //         }
+    //     }
+    // ])
+
+    // to get all patients with age greater than 18
+    const getPatients = await patientModel.aggregate([
+        {
+            $match: {
+                age:{
+                    $gt: 18
+                }
+            }
+        }
+    ])
+ 
+    
+    res.status(200).json({data:getPatients,success:true})
+}
+
+const getPatient = async (req, res, next) => {
+    const page = +req.query.page || 1;
+    const count = +req.query.count || 5;
+
+    const getPatients = await patientModel.aggregate([
+        {
+            $skip: (page - 1) * count
+        },
+        {
+            $limit: count
+        },
+        {
+            $project: {
+                name: 1,
+                age: 1,
+                disease: 1
+            }
+        }
+    ])
+    res.status(200).json({data:getPatients,success:true})
+}
+
+module.exports = {
+  createPatient: createPatient,
+  getPatient: getPatient,
+  name: "manjot",
+};
