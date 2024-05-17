a71fee56617598ef84d52b605386e28ba4ece7db
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-05-17 15:38:56 +0530

    lookup aggregation

--- services/patient.js
+++ services/patient.js
@@ -51,21 +51,33 @@ const getPatientWithAgeAggregation = async (req, res, next) => {
     // ])
 
     // to get all patients with age greater than 18
+    // const getPatients = await patientModel.aggregate([
+    //     {
+    //         $match: {
+    //             age:{
+    //                 $gt: 18
+    //             }
+    //         }
+    //     }
+    // ])
+
+    // assignedDoctor lookup
     const getPatients = await patientModel.aggregate([
         {
-            $match: {
-                age:{
-                    $gt: 18
-                }
+            $lookup: {
+                from: "doctors",
+                localField: "assignedDoctor",
+                foreignField: "_id",
+                as: "assignedDoctor"
             }
-        }
+        },
     ])
  
     
     res.status(200).json({data:getPatients,success:true})
 }
 
-const getPatient = async (req, res, next) => {
+const getPatientWithPagination = async (req, res, next) => {
     const page = +req.query.page || 1;
     const count = +req.query.count || 5;
 
@@ -88,18 +100,52 @@ const getPatient = async (req, res, next) => {
     res.status(200).json({data:getPatients,success:true})
 }
 
+const getPatientWithLookup = async (req, res, next) => {
+
+    const getPatients = await patientModel.aggregate([
+        {
+            $lookup: {
+                localField: "assignedDoctor", // of patient model
+                from: "doctors", // foreign collection (doctor model)
+                foreignField: "_id", // of doctor model
+                as: "assignedDoctor" // alias name
+            }
+        },
+        {
+            $project: {
+                name: 1,
+                age: 1,
+                disease: 1,
+                assignedDoctor: 1
+            }
+        },
+        // assignedDoctor expereince greater than 7
+        {
+            $match: {
+                "assignedDoctor.experience": {
+                    $gt: 7
+                }
+            }
+        }
+    ])
+ 
+    
+    res.status(200).json({data:getPatients,success:true})
+}
+
 const assignDoctor = async(req, res, next)=>{
     const patientId = req.params.id 
     const doctorId = req.body.doctorId 
     const patient = await patientModel.findByIdAndUpdate(patientId,{  
         assignedDoctor: new mongoose.Types.ObjectId(doctorId)
     })
-    return patient
+
+    res.status(200).json({data:patient,success:true})
 }
 
 module.exports = {
   createPatient: createPatient,
-  getPatient: getPatient,
+  getPatient: getPatientWithLookup,
   assignDoctor,
   name: "manjot",
 };
